2aa8cf02b25ae909daa34c6b93371e3ea59efb8e
Ksenia Panfilova <kseniapanfilova@gmail.com>
2024-07-25 14:35:15 +0300

    added print to test_greeting and test_circle

--- test_hw_python_1.py
+++ test_hw_python_1.py
@@ -9,6 +9,7 @@ def test_greeting():
     name = "Анна"
     age = 25
     output = f"Привет, {name}! Тебе {age} лет."
+    print(output)
 
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
@@ -37,11 +38,12 @@ def test_circle():
     """
     r = 23
     area = pi * r ** 2
+    print(f'area of circle = {area}')
 
     assert area == 1661.9025137490005
 
     length = 2 * pi * r
-
+    print(f'length of circle = {length}')
     assert length == 144.51326206513048
 
 
